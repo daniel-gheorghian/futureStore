ced0e63be5569740118b9f9a6ac9b09266cce333
daniel.gheorghian <daniel.gheorghian@mindcti.com>
2019-05-14 11:48:40 +0300

    cart controller

---
+++ backend/src/main/java/com/mind/training/futureStore/cart/CartController.java
@@ -0,0 +1,52 @@
+package com.mind.training.futureStore.cart;
+
+import com.mind.training.futureStore.cart.entity.Cart;
+import com.mind.training.futureStore.cart.service.CartService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping( "/api/cart" )
+public class CartController
+{
+    @Autowired
+    private CartService cartService;
+
+    @RequestMapping( method = RequestMethod.GET )
+    public Iterable<Cart> findAll(  )
+    {
+        return cartService.findAllCarts();
+    }
+
+    @RequestMapping( method = RequestMethod.POST )
+    public ResponseEntity<Long> addCart( @RequestBody Cart cart )
+    {
+        return new ResponseEntity<>( cartService.addCart( cart ), HttpStatus.OK );
+    }
+
+    @RequestMapping( method = RequestMethod.GET, value = "/{cartId}" )
+    public Cart getCartWithId( @PathVariable long cartId )
+    {
+        return cartService.findCartById( cartId );
+    }
+
+    @RequestMapping( method = RequestMethod.PUT, value = "/{cartId}" )
+    public Cart updateCartWithId( @PathVariable long cartId, @RequestBody Cart cart )
+    {
+        return cartService.updateCart( cartId, cart );
+    }
+
+    @RequestMapping( method = RequestMethod.PATCH, value = "/{cartId}" )
+    public Cart updateStatus( @PathVariable long cartId, @RequestParam String status )
+    {
+        return cartService.updateStatus( cartId, status );
+    }
+
+    @RequestMapping( method = RequestMethod.DELETE, value = "/{cartId}" )
+    public void deleteCart( @PathVariable long cartId )
+    {
+        cartService.deleteCart( cartId );
+    }
+}
