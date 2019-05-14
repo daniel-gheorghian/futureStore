8ed97aafc632d2f0b560168a31d8fb18c6d2b716
daniel.gheorghian <daniel.gheorghian@mindcti.com>
2019-05-14 15:19:23 +0300

    add tabs contents

--- frontend/src/app/catalogController.js
+++ frontend/src/app/catalogController.js
@@ -9,10 +9,14 @@
         $scope.catalog = [
             {
                 itemDescription: 'Mist Pods',
-                price          : 500
+                price          : 500,
+                specs: ['Comfortable to wear', 'Almost no weight', 'High fidelity', 'Available in Misty Black and Bright White'],
+                reviews: [100, 80, 99, 110]
             }, {
                 itemDescription: 'Pineapple Watch',
-                price          : 300
+                price          : 300,
+                specs: ['Accurate time piece', 'Pleasant fragrance', 'Available in Misty Black and Bright White'],
+                reviews: [60, 70, 30, 99, 80, 72]
             }];
 
         $scope.activeTab = 1;
