b2269c6af458e3414d74ed9426880baf9ba047dd
daniel.gheorghian <daniel.gheorghian@mindcti.com>
2019-05-14 11:47:23 +0300

    cart service

---
+++ backend/src/main/java/com/mind/training/futureStore/cart/service/CartService.java
@@ -0,0 +1,60 @@
+package com.mind.training.futureStore.cart.service;
+
+import com.mind.training.futureStore.cart.entity.Cart;
+import com.mind.training.futureStore.cart.repository.CartRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+@Service
+public class CartService
+{
+    @Autowired
+    private CartRepository cartRepository;
+
+    public long addCart( Cart cart )
+    {
+        Cart savedCart = cartRepository.save( cart );
+
+        return savedCart.getId( );
+    }
+
+    public Cart findCartById( long id )
+    {
+        return cartRepository.findById( id ).orElseThrow( ( ) -> new RuntimeException( "No cart with id " + id + " found!" ) );
+    }
+
+    public Cart updateCart( long id, Cart newCart )
+    {
+        Cart oldCart = cartRepository.findById( id ).orElseGet( Cart::new );
+
+        oldCart.setId( id );
+        oldCart.setAccountCode( newCart.getAccountCode( ) );
+        oldCart.setStatus( newCart.getStatus( ) );
+        oldCart.setStore( newCart.getStore( ) );
+
+        return cartRepository.save( oldCart );
+    }
+
+    public Cart updateStatus( long id, String status )
+    {
+        Cart cart = findCartById( id );
+
+        cart.setStatus( status );
+
+        return cartRepository.save( cart );
+    }
+
+    public void deleteCart( long id )
+    {
+        cartRepository.deleteById( id );
+    }
+
+    public Iterable<Cart> findAllCarts()
+    {
+        return cartRepository.findAll();
+    }
+}
