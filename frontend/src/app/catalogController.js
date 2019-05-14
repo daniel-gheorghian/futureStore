728c2a8e9a0425c214d7aa923285108fe4e01312
daniel.gheorghian <daniel.gheorghian@mindcti.com>
2019-05-14 15:06:28 +0300

    active tab logic

--- frontend/src/app/catalogController.js
+++ frontend/src/app/catalogController.js
@@ -14,6 +14,20 @@
                 itemDescription: 'Pineapple Watch',
                 price          : 300
             }];
+
+        $scope.activeTab = 1;
+        $scope.isActiveTab = isActiveTab;
+        $scope.setActiveTab = setActiveTab;
+
+        function isActiveTab ( tabNo )
+        {
+            return $scope.activeTab === tabNo;
+        }
+
+        function setActiveTab ( tabNo )
+        {
+            $scope.activeTab = tabNo;
+        }
     }
 
     angular.module( "FutureStore" ).controller( "CatalogController",
